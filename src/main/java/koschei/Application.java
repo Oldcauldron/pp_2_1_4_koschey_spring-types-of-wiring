373f913b17e38ff6dae8391dddf7d9b5c21e6a62
Atiacht <atiacht@gmail.com>
2020-12-23 20:54:47 +0300

    #1

--- src/main/java/koschei/Application.java
+++ src/main/java/koschei/Application.java
@@ -8,8 +8,7 @@ public class Application {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        KoscheiTheDeathless koscheiTheDeathless =
-                applicationContext.getBean(KoscheiTheDeathless.class);
+        KoscheiTheDeathless koscheiTheDeathless = applicationContext.getBean(KoscheiTheDeathless.class);
         System.out.println(koscheiTheDeathless.getRulesByDeth());
     }
-}
+} // #1
--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -2,6 +2,8 @@ package koschei.config;
 
 import koschei.models.Island2;
 import koschei.models.Wood3;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -10,7 +12,9 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = "koschei")
 public class AppConfig {
 
-    @Bean
+    @Bean("Island2")
+    @Autowired()
+    @Qualifier("Wood3")
     public static Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
     }
